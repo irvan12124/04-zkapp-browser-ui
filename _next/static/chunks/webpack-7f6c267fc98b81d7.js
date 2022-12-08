b0d1046f5b116b5a4cfb34d6581ab9aebbf7d405
irvan12 <irvannashrudin24@gmail.com>
2022-12-08 18:03:17 +0000

    Deploy gh-pages

--- _next/static/chunks/webpack-7f6c267fc98b81d7.js
+++ _next/static/chunks/webpack-7f6c267fc98b81d7.js
@@ -121,7 +121,7 @@
 /******/ 		// This function allow to reference all chunks
 /******/ 		__webpack_require__.miniCssF = function(chunkId) {
 /******/ 			// return url for filenames based on template
-/******/ 			return "static/css/" + "45baf844fd38bb39" + ".css";
+/******/ 			return "static/css/" + "8164a6b1c8ded876" + ".css";
 /******/ 		};
 /******/ 	}();
 /******/ 	
